1bb3de7dd50d63d13a98b45537c2633163ca8184
rudyluis <rudy.manzaneda@gmail.com>
2026-03-16 11:05:30 -0400

    16032026

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import sqlite3
+import os
 
 app = Flask(__name__)
 app.secret_key = "clave_secreta_simple"
@@ -92,4 +93,6 @@ def contacto():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    ##app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
